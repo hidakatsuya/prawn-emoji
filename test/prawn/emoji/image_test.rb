76aef2c22b60c5fb57e430af3eb595e170511466
Katsuya HIDAKA <hidakatsuya@gmail.com>
2017-06-23 02:23:40 +0900

    Add spec for Emoji::Image#path

--- test/prawn/emoji/image_test.rb
+++ test/prawn/emoji/image_test.rb
@@ -3,13 +3,19 @@
 require 'test_helper'
 
 describe Prawn::Emoji::Image do
-  let(:emojis) { %w( 😀 © 🇯🇵 ) }
+  describe '#path' do
+    let(:emojis) { %w( 😀 © 🇯🇵 ) }
 
-  it 'possible to find the image file' do
-    emojis.each do |emoji|
-      emoji_image = Prawn::Emoji::Image.new(emoji)
-
-      assert File.exist?(emoji_image.path), "#{emoji}  not found"
+    it 'possible to find the image file' do
+      emojis.each do |emoji|
+        emoji_image = Prawn::Emoji::Image.new(emoji)
+        assert File.exist?(emoji_image.path), "#{emoji}  not found"
+      end
     end
+  end
+
+  describe '#==' do
+    it { Prawn::Emoji::Image.new('🍣').must_be :==, Prawn::Emoji::Image.new('🍣') }
+    it { Prawn::Emoji::Image.new('🍣').wont_be :==, Prawn::Emoji::Image.new('🐟') }
   end
 end
