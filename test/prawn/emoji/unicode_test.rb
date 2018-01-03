6fe7c75ed1857fa48849b55c0f0baf78131c4621
Katsuya HIDAKA <hidakatsuya@gmail.com>
2018-01-04 03:02:40 +0900

    `klass` cannnot override in Minitest::Spec

--- test/prawn/emoji/unicode_test.rb
+++ test/prawn/emoji/unicode_test.rb
@@ -3,34 +3,36 @@
 require 'test_helper'
 
 describe Prawn::Emoji::Unicode do
-  let(:klass) { Prawn::Emoji::Unicode }
+  def unicode(emoji)
+    Prawn::Emoji::Unicode.new(emoji)
+  end
 
   describe '#==' do
-    it { klass.new('🐟').must_be :==, klass.new('🐟') }
-    it { klass.new('🐟').wont_be :==, klass.new('🍣') }
+    it { unicode('🐟').must_be :==, unicode('🐟') }
+    it { unicode('🐟').wont_be :==, unicode('🍣') }
   end
 
   describe '#codepoint' do
-    it { klass.new('🍣').codepoint.must_equal '1f363' }
-    it { klass.new('🇯🇵').codepoint.must_equal '1f1ef-1f1f5' }
+    it { unicode('🍣').codepoint.must_equal '1f363' }
+    it { unicode('🇯🇵').codepoint.must_equal '1f1ef-1f1f5' }
   end
 
   describe '#to_s' do
-    it { klass.new('🍣').to_s.must_equal '🍣' }
-    it { klass.new('❤️').to_s.wont_match /[\ufe0e\ufe0f]/ }
+    it { unicode('🍣').to_s.must_equal '🍣' }
+    it { unicode('❤️').to_s.wont_match /[\ufe0e\ufe0f]/ }
   end
 
   describe '#text?' do
     describe 'no selector' do
-      it { klass.new('☀').wont_be :text? }
+      it { unicode('☀').wont_be :text? }
     end
 
     describe 'with text presentation selector' do
-      it { klass.new('☀︎').must_be :text? }
+      it { unicode('☀︎').must_be :text? }
     end
 
     describe 'with emoji presentation selector' do
-      it { klass.new('☀️').wont_be :text? }
+      it { unicode('☀️').wont_be :text? }
     end
   end
 end
